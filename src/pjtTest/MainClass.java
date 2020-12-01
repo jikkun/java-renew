cd8effc09675dd032c8bb82e0daa7807cef3ba2d
jikkun <henjuck@gmail.com>
2020-12-01 23:53:08 +0900

    first commit

---
+++ src/pjtTest/MainClass.java
@@ -0,0 +1,18 @@
+package pjtTest;
+
+public class MainClass {
+    public static void main(String[] args) {
+        int i =10;
+        System.out.println("i = " + i);
+
+        i = 100;
+        System.out.println("i = " + i);
+
+        i = 3000;
+        System.out.println("i = " + i);
+
+        i = 00;
+        System.out.println("i = " + i);
+    }
+    
+}
